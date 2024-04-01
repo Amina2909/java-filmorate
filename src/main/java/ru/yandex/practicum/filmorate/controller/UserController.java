582d110061332e71d3c4fcfd83724ef5fc1a03d4
Amina <ami2909@yandex.ru>
2024-04-01 22:34:32 +0300

    добавлены тесты валидации и исправленны недочеты

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.UpdateException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
@@ -53,9 +52,6 @@ public class UserController {
     public void nameEqualsLogin(User user) {
         if ((user.getName() == null) || (user.getName().isBlank())) {
             user.setName(user.getLogin());
-        }
-        if (user.getLogin().contains(" ")) {
-            throw new ValidationException("The login must not contain gaps");
         }
     }
 
